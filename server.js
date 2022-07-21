180015457cb677ed44c7373ebc42e9b1e1166bc8
samsiebert <sammsiebert@gmail.com>
2022-07-21 15:47:00 -0500

    post route pushes new notes to db.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const  notes  = require('./Develop/db/db.json');
+const notes  = require('./Develop/db/db.json');
 
 const app = express();
 
@@ -10,12 +11,25 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+
+    return note;
+}
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
     res.json(req.body);
 
 })
